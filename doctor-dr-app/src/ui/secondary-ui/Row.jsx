abfb382bee4a6f73ec329c4c47a00ca384035acd
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-02-03 17:47:15 +0530

    feat: adding a row related ccs customization.

--- doctor-dr-app/src/ui/secondary-ui/Row.jsx
+++ doctor-dr-app/src/ui/secondary-ui/Row.jsx
@@ -4,18 +4,27 @@ const Row = styled.div`
   display: flex;
 
   ${(props) =>
-    props.type === 'horizontal' &&
+    props.type === "horizontal" &&
     css`
       justify-content: space-between;
       align-items: center;
     `}
 
   ${(props) =>
-    props.type === 'vertical' &&
+    props.type === "vertical" &&
     css`
       flex-direction: column;
       gap: 1.6rem;
     `}
+    
+  ${(props)=>
+    props.type === 'horizontal-start' &&
+     css`
+      justify-content: flex-start;
+      align-items: center;
+      gap: 5px;
+    `}
+    
 `;
 
 Row.defaultProps = {
