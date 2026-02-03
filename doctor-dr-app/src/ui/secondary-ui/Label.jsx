6977ce87b7d976b754242ad59c3f23055ac700a6
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-02-03 17:46:45 +0530

    feat: custmizing css with variables.

--- doctor-dr-app/src/ui/secondary-ui/Label.jsx
+++ doctor-dr-app/src/ui/secondary-ui/Label.jsx
@@ -1,7 +1,12 @@
-import styled from 'styled-components';
+import styled ,{ css }from 'styled-components';
 
 const Label = styled.label`
   font-weight: 500;
+  ${(props) =>
+    props.type === 'overflow-break-word' &&
+    css`
+      overflow-wrap: break-word;
+    `}
 `;
 
 export default Label;
