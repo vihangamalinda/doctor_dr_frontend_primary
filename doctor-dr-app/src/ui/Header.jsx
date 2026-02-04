af1146a54df27cc6af3359518934727de4fc6fe4
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-02-04 12:22:37 +0530

    feat: Header  ui component.

--- doctor-dr-app/src/ui/Header.jsx
+++ doctor-dr-app/src/ui/Header.jsx
@@ -2,13 +2,14 @@ import { styled } from 'styled-components';
 
 
 const StyledHeader = styled.header`
+  font-weight: 900;
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
 `;
 
 function Header() {
-  return <StyledHeader>Header</StyledHeader>;
+  return <StyledHeader>Doctor DR</StyledHeader>;
 }
 
 export default Header;
