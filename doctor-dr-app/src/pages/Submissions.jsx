9e05521adb4fb2afff29ccf5a8a9f2bf7478a2ed
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-02-04 12:22:03 +0530

    feat: Submission page.

--- doctor-dr-app/src/pages/Submissions.jsx
+++ doctor-dr-app/src/pages/Submissions.jsx
@@ -2,17 +2,17 @@ import SubmissionTable from '../features/submission/SubmissionTable';
 import Heading from '../ui/secondary-ui/Heading';
 import Row from '../ui/secondary-ui/Row';
 
-function Submissions(){
-    return (
-        <>
-         <Row type="horizontal">
+function Submissions() {
+  return (
+    <>
+      <Row type="horizontal">
         <Heading as="h1">Submissions by user</Heading>
         <p>Filter/Sort</p>
       </Row>
       <Row>
         <SubmissionTable />
       </Row>
-        </>
-    )
+    </>
+  );
 }
 export default Submissions;
