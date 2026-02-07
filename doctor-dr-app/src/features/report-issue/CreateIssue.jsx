95b7efb536cf003bc7ca47a5b895bee29eb6af74
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-02-07 17:01:30 +0530

    feat: Adding logic to creation issue  component.

--- doctor-dr-app/src/features/report-issue/CreateIssue.jsx
+++ doctor-dr-app/src/features/report-issue/CreateIssue.jsx
@@ -4,12 +4,51 @@ import FormRow from "../../ui/secondary-ui/FormRow";
 import Label from "../../ui/secondary-ui/Label"
 import Textarea from '../../ui/secondary-ui/TextArea';
 import Input from '../../ui/secondary-ui/Input';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+import {createReportIssue} from "../../services/apis/apiReportIssue.js";
+import Button from '../../ui/secondary-ui/Button.jsx';
 
+const createLocalDateTime =()=>{
+  const now = new Date();
+
+  const localDateTime = now
+  .toISOString()
+  .slice(0, 19);
+  return localDateTime;
+}
+
+const createReportedIssueData = (data, userProfileId) => {
+  const { title, description } = data;
+
+  return {
+    title,
+    description,
+    userProfile: {
+      id: userProfileId,
+    },
+    createdDateTime: createLocalDateTime(),
+  };
+};
+const userProfileId =6;
 function CreateIssue(){
     const { register, handleSubmit } = useForm();
+    const quertClient =useQueryClient();
+    const {mutate,isLoading} = useMutation({
+      mutationFn:createReportIssue,
+      onSuccess:()=>{
+        toast.success("Create an issue");
+        quertClient.invalidateQueries({queryKey:["reported-issues"]})
+      },
+      onError:(err)=>{
+        toast.error(err);
+      }
+    })
 
     function onSubmit(data){
         console.log("data",data);
+        const reportedIssueData =createReportedIssueData(data,userProfileId);
+        mutate(reportedIssueData);
     }
 
     return (
@@ -28,6 +67,11 @@ function CreateIssue(){
             defaultValue=""
             {...register("description")}
           />
+        </FormRow>
+        <FormRow>
+          <Button variation="primary" size="large" type="submit">
+            Submit
+          </Button>
         </FormRow>
       </Form>
     );
