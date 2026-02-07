9ae90fbedc152e513926aa161c122e9890d06119
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-02-07 17:00:30 +0530

    feat: Adding a function to format the address.

--- doctor-dr-app/src/features/location/LocationInformation.jsx
+++ doctor-dr-app/src/features/location/LocationInformation.jsx
@@ -1,18 +1,21 @@
 import Label from "../../ui/secondary-ui/Label";
 import Row from "../../ui/secondary-ui/Row";
 
+const getAddress = (location)=>{
+    const {locationIndex,road,city,country} =location;
+    return `${locationIndex}/${road}/${city}/${country}`;
+}
 function LocationInformation({location}){
-        const {address,postalCode} =location;
 
     return (
         <Row type="vertical">
             <Row type="horizontal-start">
                 <Label>Address : </Label>
-                 <Label>{address}</Label>
+                 <Label>{getAddress(location)}</Label>
             </Row>
             <Row type="horizontal-start">
                 <Label>Postal Code : </Label>
-                 <Label>{postalCode}</Label>
+                 <Label>{location.postalCode}</Label>
             </Row>
         </Row>
     )
