6887dd6fdae1ad88b3fed7c7005163b34df16a93
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-02-07 18:09:44 +0530

    feat: integrating display feedback compoent.

--- doctor-dr-app/src/features/request-help/RequestCard.jsx
+++ doctor-dr-app/src/features/request-help/RequestCard.jsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 import Label from "../../ui/secondary-ui/Label";
 import Row from "../../ui/secondary-ui/Row";
+import LabelTextArea from '../../ui/request-form-ui/LabelTextArea';
+import LabelPrimary from '../../ui/request-form-ui/LabelPrimary';
+ import LabelGroupStyle from '../../ui/request-form-ui/LabelGroupStyle';
+import DisplayFeedback from './DisplayFeedback';
 
 const RequestCardStyle = styled.div`
   display: flex;
@@ -12,25 +16,9 @@ const RequestCardStyle = styled.div`
   background-color:var(--color-grey-200);
   border-bottom: 1px solid var(--color-grey-400);
   color: var(--color-grey-600);
+  `
     
-   
-`
-const LabelGroupStyle =styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1.6rem;
-  padding:10px 20px;
-  border:1px solid var(--color-grey-300);
-  border-radius:20px;
-  background-color:var(--color-grey-100);
-`
-const LabelPrimary =styled.label`
-  font-weight: 700;
-`
-const LabelTextArea =styled.label`
-  font-weight:100;
-  overflow-wrap: break-word;
-`
+
 const HeadingGrouping=styled.div`
   display: flex;
   justify-content: flex-start;
@@ -48,13 +36,12 @@ const LabelTitleSecondary =styled.label`
   font-weight:900;
   overflow-wrap: break-word;
 `
-const SubLabel =styled.label`
- font-weight:300;
-`;
+
 
 function RequestCard({ request }) {
-  const {title, description, feedback, contactInfor } = request;
-  const { contactNumber, name } = contactInfor;
+  const {title, description, feedback, contactName,contactNumber, status} = request;
+  const hasGivenFeedBack = status.id !== 10003;
+  
   return (
 
     <RequestCardStyle >
@@ -66,23 +53,8 @@ function RequestCard({ request }) {
         <LabelPrimary>Description</LabelPrimary>
         <LabelTextArea>{description}</LabelTextArea>
       </LabelGroupStyle>
-      <LabelGroupStyle>
-        <LabelPrimary>Feedback </LabelPrimary>
-        <LabelTextArea>{feedback}</LabelTextArea>
-      </LabelGroupStyle>
-      <LabelGroupStyle>
-        <LabelPrimary>Contact Information</LabelPrimary>
-        <Row type="horizontal-start">
-          <SubLabel>Contact Name :</SubLabel>
-          <SubLabel>{name}</SubLabel>
-        </Row>
-        <Row type="horizontal-start">
-          <SubLabel>Contact Number :</SubLabel>
-          <SubLabel>{contactNumber}</SubLabel>
-        </Row>
-      </LabelGroupStyle>
+       {hasGivenFeedBack && <DisplayFeedback feedback={feedback} contactName={contactName} contactNumber={contactNumber}/>}
     </RequestCardStyle>
-    // </Row>
   );
 }
 export default RequestCard;
