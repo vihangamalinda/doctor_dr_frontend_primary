dbc83e0ffabaa35d491a2119e7ed15cd0f3df68d
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-02-07 17:10:24 +0530

    feat: integrating react-toaster to  display notification.

--- doctor-dr-app/src/App.jsx
+++ doctor-dr-app/src/App.jsx
@@ -16,6 +16,7 @@ import Login from "./pages/Login";
 import PageNotFound from './pages/PageNotFound';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Toaster } from 'react-hot-toast';
 
 
 const queryClient =new QueryClient({
@@ -27,13 +28,29 @@ const queryClient =new QueryClient({
   },
 });
 
+const toastOptions = {
+  success: {
+    duration: 3000,
+  },
+  error: {
+    duration: 5000,
+  },
+  style: {
+    fontSize: "16px",
+    maxWidth: "500px",
+    padding: "16px 24px",
+    backgroundColor: "var(--color-grey-0)",
+    color: "var(--color-grey-700)",
+  },
+};
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-      <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false}/>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -47,14 +64,17 @@ function App() {
             <Route path="user" element={<User />} />
             <Route path="request-helper" element={<Request />} />
           </Route>
-             <Route path="login" element={<Login />} />
-        <Route path="*" element={<PageNotFound />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
-
-     
-
       </BrowserRouter>
-      </QueryClientProvider>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={toastOptions}
+      />
+    </QueryClientProvider>
   );
 }
 
