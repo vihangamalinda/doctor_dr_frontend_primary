797eaa69c005479d11c870d8fae0cc2355df5912
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-03-07 21:35:38 +0530

    feat: adding a form to register operational support staff.

---
+++ doctor-dr-app/src/features/user-profile/creationalForms/CreateOperationalStaffUserProfile.jsx
@@ -0,0 +1,57 @@
+import { useForm, FormProvider } from "react-hook-form";
+import Form from "../../../ui/secondary-ui/Form.jsx";
+import CustomFormRow from "../../../ui/secondary-ui/CustomFormRow.jsx";
+import Button from "../../../ui/secondary-ui/Button.jsx";
+import Selector from "../../../ui/secondary-ui/Selector.jsx";
+import UserProfilePersonalInformationForm from "./UserProfilePersonalInformationForm.jsx";
+import { createLocalDateTime } from "../../shared/DateTimeHelper.js";
+import { useInternalSystemHospital } from "../../../hooks/useInternalSystemHospital.js";
+import Spinner from "../../../ui/secondary-ui/Spinner.jsx";
+
+function CreateOperationalStaffUserProfile() {
+  const methods = useForm();
+  const { register, handleSubmit, formState } = methods;
+  const { errors } = formState;
+
+  const {
+    allInternalSystemHospital,
+    isAllInternalSystemHospitalLoading,
+    error,
+  } = useInternalSystemHospital();
+
+  const onSubmit = (data) => {
+    const currentDateTime = createLocalDateTime();
+    data.dateTime = currentDateTime;
+    console.log(data);
+    console.log(errors);
+  };
+  const onError = () => {};
+
+  if (isAllInternalSystemHospitalLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <FormProvider {...methods}>
+      <Form onSubmit={handleSubmit(onSubmit, onError)}>
+        <UserProfilePersonalInformationForm />
+        <CustomFormRow
+          label="Select Responsible Hospital"
+          error={errors?.hospitalId?.message}
+        >
+          <Selector
+            identifier="hospitalId"
+            optionList={allInternalSystemHospital}
+            requiredMessage="Hospital should be selected"
+          ></Selector>
+        </CustomFormRow>
+        <CustomFormRow>
+          <Button variation="primary" size="large" type="submit">
+            Submit
+          </Button>
+        </CustomFormRow>
+      </Form>
+    </FormProvider>
+  );
+}
+export default CreateOperationalStaffUserProfile;
