7f63b166811e2533b85b2e7561f3843b42d37909
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-03-07 12:06:50 +0530

    feat: Adding a custom selector component.

---
+++ doctor-dr-app/src/ui/secondary-ui/Selector.jsx
@@ -0,0 +1,35 @@
+import { useFormContext } from "react-hook-form";
+import styled from "styled-components";
+
+const SelectorStyled = styled.select`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+
+  width: 100%;
+  font-size: 1.15rem;
+  padding: 0.675em 1em;
+  background-color: #fff;
+  border: 1px solid #caced1;
+  border-radius: 0.25rem;
+  color: #000;
+  cursor: pointer;
+`;
+
+function Selector({ identifier, requiredMessage, optionList }) {
+  const { register, formState } = useFormContext();
+  const { errors } = formState;
+
+  return (
+    <SelectorStyled {...register(identifier, { required: requiredMessage })}>
+      <option value="">Select Hospital</option>
+      {optionList.map((option) => (
+        <option key={option.id} value={option.name}>
+          {option.name}
+        </option>
+      ))}
+    </SelectorStyled>
+  );
+}
+
+export default Selector;
