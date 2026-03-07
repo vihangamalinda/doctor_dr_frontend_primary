39dd8af8fbf76c00a8165aad243bd627bee64525
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-03-07 21:35:10 +0530

    feat: Adding a common user profile form.

---
+++ doctor-dr-app/src/features/user-profile/creationalForms/CreateCommonUserProfile.jsx
@@ -0,0 +1,56 @@
+import { useForm, FormProvider } from "react-hook-form";
+import Form from "../../../ui/secondary-ui/Form.jsx";
+import CustomFormRow from "../../../ui/secondary-ui/CustomFormRow.jsx";
+import Button from "../../../ui/secondary-ui/Button.jsx";
+import Selector from "../../../ui/secondary-ui/Selector.jsx";
+import UserProfilePersonalInformationForm from "./UserProfilePersonalInformationForm.jsx";
+import { createLocalDateTime } from "../../shared/DateTimeHelper.js";
+import { useRegularHospital } from "../../../hooks/useRegularHospital.js";
+import Spinner from "../../../ui/secondary-ui/Spinner.jsx";
+
+function CreateCommonUserProfile() {
+  const methods = useForm();
+  const { register, handleSubmit, formState } = methods;
+  const { errors } = formState;
+
+  const { allRegularHospital, isAllRegularHospitalLoading, error } =
+    useRegularHospital();
+
+  const onSubmit = (data) => {
+    const currentDateTime = createLocalDateTime();
+    data.dateTime = currentDateTime;
+    console.log(data);
+    console.log(errors);
+  };
+  const onError = () => {};
+
+  if (isAllRegularHospitalLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <FormProvider {...methods}>
+      <Form onSubmit={handleSubmit(onSubmit, onError)}>
+        <UserProfilePersonalInformationForm />
+
+        <CustomFormRow
+          label="Select Responsible Hospital"
+          error={errors?.hospitalId?.message}
+        >
+          <Selector
+            identifier="hospitalId"
+            optionList={allRegularHospital}
+            requiredMessage="Hospital should be selected"
+          ></Selector>
+        </CustomFormRow>
+        <CustomFormRow>
+          <Button variation="primary" size="large" type="submit">
+            Submit
+          </Button>
+        </CustomFormRow>
+      </Form>
+    </FormProvider>
+  );
+}
+
+export default CreateCommonUserProfile;
