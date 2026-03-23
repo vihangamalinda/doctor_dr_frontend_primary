c501c41e7b26f714c2f48a25a675ea6fc0311f40
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-03-24 01:00:37 +0530

    feat: API call specifications related user credentials.

---
+++ doctor-dr-app/src/services/apis/apiUserCredentials.js
@@ -0,0 +1,21 @@
+import { USER_CREDENTIALS_API_URLs } from "../apiConfigs";
+import { getData, createDataAndReturnResponse } from "../apiHelper";
+
+const getAllUserNames = async () => {
+  return await getData(
+    USER_CREDENTIALS_API_URLs.USER_CREDENTIALS_PERFORM_GET_ALL_USER_NAMES,
+  );
+};
+
+const createJwtTokenByUserLogin = async (userCredentials) => {
+  console.log("api ", USER_CREDENTIALS_API_URLs.USER_CREDENTIALS_PERFORM_LOGIN);
+  const data = {
+    obj: userCredentials,
+    url: USER_CREDENTIALS_API_URLs.USER_CREDENTIALS_PERFORM_LOGIN,
+  };
+  // const response=await createDataAndReturnResponse(data);
+  // console.log(response);
+  return await createDataAndReturnResponse(data);
+};
+
+export { getAllUserNames, createJwtTokenByUserLogin };
