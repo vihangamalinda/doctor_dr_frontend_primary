959f5eab526ade1ef786c680eddd50fde9a0ae33
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-03-26 11:14:30 +0530

    feat: Adding protected route component to handle user behaviour.

---
+++ doctor-dr-app/src/ui/ProtecctedRoute.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { useCurrentLoggedUserInformation } from "../features/authentication/hooks/useCurrentLoggedUserInformation.js";
+import Spinner from "./secondary-ui/Spinner.jsx";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const FullPage= styled.div`
+    height: 100vh;
+    background-color: var(--color-grey-0);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+
+function ProtectedRoute({ children }) {
+    const navigate = useNavigate();
+
+    // Authenticated user
+     const { currentUser, isAuthenticating,isAuthenticated, error } =
+       useCurrentLoggedUserInformation();
+
+
+    console.log(isAuthenticated, isAuthenticating);
+    // if the user is not authenticated, navigate to login page
+    useEffect(()=>{
+        if(!isAuthenticated && !isAuthenticating){
+            navigate("/login");
+        }
+    },[navigate,isAuthenticated,isAuthenticating])
+
+    
+    // and spiner while authenticating
+    if(isAuthenticating ){
+        return <FullPage><Spinner/></FullPage>;
+    }
+
+    // if authenticated, return the children routes 
+  
+
+   if(isAuthenticated) return children;
+}
+export default ProtectedRoute;
