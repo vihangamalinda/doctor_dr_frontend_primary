0cf54d61adc3df87ceb7c68b7ff8783937b9f1ac
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-06 16:57:11 +0530

    feat:  custom hook responsible for updating the currenlty logged user on redux.

--- doctor-dr-app/src/features/authentication/hooks/useCurrentLoggedUserInformation.js
+++ doctor-dr-app/src/features/authentication/hooks/useCurrentLoggedUserInformation.js
@@ -1,11 +1,22 @@
 import { useQuery } from "@tanstack/react-query";
 import { getCurrentLoggedUser as getCurrentLoggedUserApi } from "../../../services/apis/apiUserCredentials.js";
+import { useDispatch } from "react-redux";
+import { updateCurrentLoggedUser } from "../store/useCurrentLoggedUser.js";
 
 export function useCurrentLoggedUserInformation() {
-  const {data:currentUser,isLoading: isAuthenticating,error} =useQuery({
+  const {
+    data: currentUser,
+    isLoading: isAuthenticating,
+    error,
+  } = useQuery({
     queryKey: ["currentLoggedUser"],
     queryFn: getCurrentLoggedUserApi,
   });
+  const dispatch = useDispatch();
+
+  if (!isAuthenticating && !error && currentUser) {
+    dispatch(updateCurrentLoggedUser(currentUser));
+  }
 
   return {
     currentUser,
