f4ddd050b017744bb98990cbc955663053d999de
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-06 16:58:38 +0530

    feat:  Slice responsible for storing current logged user.

---
+++ doctor-dr-app/src/features/authentication/store/useCurrentLoggedUser.js
@@ -0,0 +1,27 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    userName: "",
+    authorities: [],
+    accountNonExpired: false,
+    authenticated: false,
+    userProfileId: null,
+};
+
+const currentLoggedUserSlice = createSlice({
+    name: "currentLoggedUser",
+    initialState,
+    reducers: {
+      updateCurrentLoggedUser: (state, action) => { 
+        state.userName = action.payload.userName;
+        state.authorities = action.payload.authorities;
+        state.accountNonExpired = action.payload.accountNonExpired;
+        state.authenticated = action.payload.authenticated;
+        state.userProfileId = action.payload.userProfileId;
+      }
+    }
+});
+
+export const { updateCurrentLoggedUser } = currentLoggedUserSlice.actions;
+
+export default currentLoggedUserSlice.reducer;
