d52a762499de4f6f0de4db48b38c2037c70fb96a
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-10 04:08:01 +0530

    refactor: Refactoring User page logic to feature sections.

---
+++ doctor-dr-app/src/features/user-profile/DisplayCurrentUserProfileInformation.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from "react-redux";
+import { selectCurrentLoggedUserProfileId } from "../authentication/store/selectors/CurrentLoggedUserSelectors";
+import { useUserProfileById } from "./hooks/useUserProfileById";
+import UserProfileInformation from "./UserProfileInformation";
+import Spinner from "../../ui/secondary-ui/Spinner";
+import HospitalInformation from "../hospital/HospitalInformation";
+
+function DisplayCurrentUserProfileInfromation() {
+  const currentLoggedInUserProfileId = useSelector(
+    selectCurrentLoggedUserProfileId,
+  );
+  const { userProfileDataById, isUserProfileLoading } = useUserProfileById(
+    currentLoggedInUserProfileId,
+  );
+
+  if (isUserProfileLoading) return <Spinner />;
+
+  return (
+    <>
+      <UserProfileInformation user={userProfileDataById} />
+      <p> Hospital information </p>
+      <HospitalInformation hospital={userProfileDataById.hospital} />
+    </>
+  );
+}
+export default DisplayCurrentUserProfileInfromation;
--- doctor-dr-app/src/pages/User.jsx
+++ doctor-dr-app/src/pages/User.jsx
@@ -1,23 +1,11 @@
-import UserProfileInformation from "../features/user-profile/UserProfileInformation";
-import HospitalInformation from "../features/hospital/HospitalInformation";
 import Heading from "../ui/secondary-ui/Heading";
-import Spinner from "../ui/secondary-ui/Spinner.jsx";
-import { useUserProfileById } from "../features/user-profile/hooks/useUserProfileById.js";
-
-const userProfileId = 6;
+import DisplayCurrentUserProfileInfromation from "../features/user-profile/DisplayCurrentUserProfileInformation.jsx";
 
 function User() {
-  const { userProfileDataById, isUserProfileLoading } =
-    useUserProfileById(userProfileId);
-
-  if (isUserProfileLoading) return <Spinner />;
-
   return (
     <>
       <Heading as="h1"> User details</Heading>
-      <UserProfileInformation user={userProfileDataById} />
-      <p> Hospital information </p>
-      <HospitalInformation hospital={userProfileDataById.hospital} />
+      <DisplayCurrentUserProfileInfromation/>
     </>
   );
 }
