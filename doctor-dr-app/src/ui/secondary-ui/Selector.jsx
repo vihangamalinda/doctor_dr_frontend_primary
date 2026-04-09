ab77e9e6a2dce6f2c0f679efb7041ac3ba641e43
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-10 03:23:05 +0530

    feat: Adding  Selector compoent that does not depend on Form.

--- doctor-dr-app/src/ui/secondary-ui/Selector.jsx
+++ doctor-dr-app/src/ui/secondary-ui/Selector.jsx
@@ -1,34 +1,30 @@
-import { useFormContext } from "react-hook-form";
 import styled from "styled-components";
 
-const SelectorStyled = styled.select`
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-
-  width: 100%;
-  font-size: 1.15rem;
-  padding: 0.675em 1em;
-  background-color: #fff;
-  border: 1px solid #caced1;
-  border-radius: 0.25rem;
-  color: #000;
-  cursor: pointer;
+const StyledSelector = styled.select`
+  font-size: 1.4rem;
+  padding: 0.8rem 1.2rem;
+  border: 1px solid
+    ${(props) =>
+      props.type === "white"
+        ? "var(--color-grey-100)"
+        : "var(--color-grey--300)"};
+  border-radius: var(--border--radius-sm);
+  background-color: var(--color-grey-0);
+  font-weight: 500;
+  box-shadow: var(--shadow-sm);
 `;
 
-function Selector({ identifier, requiredMessage, optionList }) {
-  const { register, formState } = useFormContext();
-  const { errors } = formState;
-
+function Selector({ options, value, onChange, ...props }) {
   return (
-    <SelectorStyled {...register(identifier, { required: requiredMessage })}>
-      <option value="">Select Hospital</option>
-      {optionList.map((option) => (
-        <option key={option.id} value={option.name}>
-          {option.name}
-        </option>
-      ))}
-    </SelectorStyled>
+    <StyledSelector onChange={onChange} value={value} {...props}>
+      {options.map((selectorOption) => {
+        return (
+          <option value={selectorOption.value} key={selectorOption.value}>
+            {selectorOption.label}
+          </option>
+        );
+      })}
+    </StyledSelector>
   );
 }
 
