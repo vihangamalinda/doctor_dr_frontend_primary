58da6882f65916506987f4a57fc34a2de8314b07
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-12 04:32:49 +0530

    feat: exposing the custom navigation link components.

---
+++ doctor-dr-app/src/ui/navigation-ui/navigation-link-ui/index.js
@@ -0,0 +1,27 @@
+import ToAddCommonUserProfile from "./ToAddCommonUserProfile";
+import ToAddOperationalStaffUserProfile from "./ToAddOperationalStaffUserProfile";
+import ToAddReportedIsusse from "./ToAddReportedIssue";
+import ToAddRequestedHelp from "./ToAddRequestedHelp";
+import ToDashboard from "./ToDashboard";
+import ToDiagnosis from "./ToDiagnosis";
+import ToOperationalStaffUserProfiles from "./ToOperationalStaffUserProfiles";
+import ToRegisteredUserProfiles from "./ToRegisteredUserProfiles";
+import ToReportedIsusses from "./ToReportedIssues";
+import ToSettings from "./ToSettings";
+import ToSubmissions from "./ToSubmissions";
+import ToUserProfile from "./ToUserProfile";
+
+export {
+  ToAddCommonUserProfile,
+  ToAddOperationalStaffUserProfile,
+  ToAddReportedIsusse,
+  ToAddRequestedHelp,
+  ToDashboard,
+  ToDiagnosis,
+  ToOperationalStaffUserProfiles,
+  ToRegisteredUserProfiles,
+  ToReportedIsusses,
+  ToSettings,
+  ToSubmissions,
+  ToUserProfile,
+};
