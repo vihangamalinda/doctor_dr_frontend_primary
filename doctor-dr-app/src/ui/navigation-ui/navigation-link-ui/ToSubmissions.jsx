c4b58f7983dc192a2fb0504fb2516d312289e3c0
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-12 04:33:19 +0530

    fix: missed navigation component link.

---
+++ doctor-dr-app/src/ui/navigation-ui/navigation-link-ui/ToSubmissions.jsx
@@ -0,0 +1,16 @@
+import StyledNavLink from "./StyledNavLink.jsx";
+import {TO_SUBMISSIONS_PAGE} from "./NavigationConstants.js";
+import {
+  HiMiniChartBarSquare,
+} from "react-icons/hi2";
+
+function ToSubmissions(){
+    return (
+              <StyledNavLink to={TO_SUBMISSIONS_PAGE}>
+                <HiMiniChartBarSquare /> <span>Submissions</span>
+              </StyledNavLink>
+    )
+
+}
+
+export default ToSubmissions;
