4d05e19cfc1e1f6facc9f10fa2873143137c749c
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-12 04:30:18 +0530

    feat: defining a new selector to retrive authorities.

--- doctor-dr-app/src/features/authentication/store/selectors/CurrentLoggedUserSelectors.js
+++ doctor-dr-app/src/features/authentication/store/selectors/CurrentLoggedUserSelectors.js
@@ -2,3 +2,5 @@ export const selectCurrentLoggedUser = (state) => state.currentLoggedUser;
 
 export const selectCurrentLoggedUserProfileId = (state) =>
   state.currentLoggedUser.userProfileId;
+
+export const selectCurrentLoggedUserProfileAuthorities = (state)=> state.currentLoggedUser.authorities
