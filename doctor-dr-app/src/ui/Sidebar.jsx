3e52b3e08cf6d10d1d64dfcc6386cee3bb5b1361
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-12 04:31:06 +0530

    fix: fixing the MainNav import.

--- doctor-dr-app/src/ui/Sidebar.jsx
+++ doctor-dr-app/src/ui/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { styled } from "styled-components";
-import MainNav from "./MainNav";
+import MainNav from "./navigation-ui/MainNav";
 import Logo from "./Logo";
 
 const StyledSidebar = styled.aside`
