1f94819a4876647d63bacb2fee9e0e62c3041a21
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-14 02:58:08 +0530

    feat: Page for reviewing the requested helps.

---
+++ doctor-dr-app/src/pages/ReviewRequestedHelps.jsx
@@ -0,0 +1,15 @@
+/*
+    this page is belong to Operational-staff
+*/
+import ReviewRequestedHelpsTable from "../features/request-help/ReviewRequestedHelpsTable";
+import Heading from "../ui/secondary-ui/Heading";
+
+function ReviewRequestedHelps() {
+  return (
+    <>
+      <Heading as="h1">Review Requested Helps</Heading>
+      <ReviewRequestedHelpsTable />
+    </>
+  );
+}
+export default ReviewRequestedHelps;
