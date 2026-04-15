c384f9c1b01b2f3d95131361b0c78a9e2cd85d6d
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-15 07:05:48 +0530

    feat: definging Detail Requested Help page.

---
+++ doctor-dr-app/src/pages/DetailRequestedHelp.jsx
@@ -0,0 +1,9 @@
+import RequestHelpDetailsContainer from "../features/request-help/RequestHelpDetailsContainer";
+
+function DetailRequestedHelp(){
+    return (
+        <RequestHelpDetailsContainer/>
+    );
+}
+
+export default DetailRequestedHelp;
