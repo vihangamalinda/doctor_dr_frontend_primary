24308f0d36093be1ab365d11a3cf75b14f0f3344
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-15 07:13:04 +0530

    feat: adding a new feature to custom form row.

--- doctor-dr-app/src/ui/secondary-ui/CustomFormRow.jsx
+++ doctor-dr-app/src/ui/secondary-ui/CustomFormRow.jsx
@@ -1,13 +1,26 @@
 import styled from "styled-components";
 import Label from "./Label";
 import FormRow from "./FormRow";
+import Row from "./Row";
 
 const Error = styled.span`
   font-size: 1.4rem;
   color: var(--color-red-700);
 `;
 
-function CustomFormRow({ label, error, children }) {
+// const 
+
+function CustomFormRow({ label, error, children ,isVerticalView}) {
+  if(isVerticalView){
+    return (
+      <Row type="vertical">
+         {label && <Label htmlFor={children.props.id}>{label}</Label>}
+      {children}
+      {error && <Error>{error}</Error>}
+      </Row>
+    )
+  }
+
   return (
     <FormRow>
       {label && <Label htmlFor={children.props.id}>{label}</Label>}
