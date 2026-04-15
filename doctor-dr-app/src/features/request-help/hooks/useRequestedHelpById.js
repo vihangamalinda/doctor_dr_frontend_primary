0194201eacde621e2334356d56d3bd90a6c3ccee
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-15 07:08:23 +0530

    feat: defining a custom hook to retrive Requested helps by id.

---
+++ doctor-dr-app/src/features/request-help/hooks/useRequestedHelpById.js
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { getRequestHelpById as getRequestHelpByIdApi } from "../../../services/apis/apiRequestHelp.js";
+import {REQUESTED_HELP_BY_ID} from "./queryKeys.js";
+
+export function useRequestedHelpById(requestedHelpId) {
+  const {
+    data: requestedHelpById,
+    isLoading: isRequestedHelpLoading,
+    error,
+  } = useQuery({
+    queryKey: REQUESTED_HELP_BY_ID(requestedHelpId),
+    queryFn: () =>
+      getRequestHelpByIdApi(requestedHelpId),
+  });
+  return {
+    requestedHelpById,
+    isRequestedHelpLoading,
+    error,
+  };
+}
