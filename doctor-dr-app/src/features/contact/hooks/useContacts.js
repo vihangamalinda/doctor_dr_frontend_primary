3cbd511b6b542903f3e8b781b768c77b3f65643b
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-16 06:54:16 +0530

    feat:  retrive all the contacts using custom hook.

---
+++ doctor-dr-app/src/features/contact/hooks/useContacts.js
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAllContacts as getAllContactsApi } from "../../../services/apis/apiContact.js";
+import {GET_ALL_CONTACTS} from "./queryKeys.js";
+
+export function useContacts() {
+  const {
+    data: allContacts,
+    isLoading: isAllContactsLoading,
+    error,
+  } = useQuery({
+    queryKey: GET_ALL_CONTACTS,
+    queryFn: () => getAllContactsApi(),
+  });
+
+  return {
+    allContacts,
+    isAllContactsLoading,
+    error,
+  };
+}
