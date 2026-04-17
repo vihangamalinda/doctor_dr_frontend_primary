54bd85a61b76408c88b95fd850add8865464dd75
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-17 12:09:23 +0530

    feat: Adding the logic to creating a common user.

--- doctor-dr-app/src/features/user-profile/creationalForms/CreateCommonUserProfile.jsx
+++ doctor-dr-app/src/features/user-profile/creationalForms/CreateCommonUserProfile.jsx
@@ -4,36 +4,46 @@ import CustomFormRow from "../../../ui/secondary-ui/CustomFormRow.jsx";
 import Button from "../../../ui/secondary-ui/Button.jsx";
 import SelectorWithinForm from "../../../ui/secondary-ui/SelectorWithinForm.jsx";
 import UserProfilePersonalInformationForm from "./UserProfilePersonalInformationForm.jsx";
-import { createLocalDateTime } from "../../shared/DateTimeHelper.js";
 import { useRegularHospital } from "../../../hooks/useRegularHospital.js";
 import Spinner from "../../../ui/secondary-ui/Spinner.jsx";
+import Input from "../../../ui/secondary-ui/Input.jsx";
+import UserProfileAccountInformationForm from "./UserProfileAccountInformationForm.jsx";
+import CreateUserProfileForm from "./CreateUserProfileForm.jsx";
+import {useCommonUserRole} from "../../user-role/helper/index.js"
+import {createData} from "./creationalData.js";
+import { useCreateUserProfile } from "../hooks/useCreateUserProfile.js";
+import toast from "react-hot-toast";
 
 function CreateCommonUserProfile() {
   const methods = useForm();
-  const { register, handleSubmit, formState } = methods;
+  const { register, handleSubmit, formState,reset } = methods;
   const { errors } = formState;
 
-  const { allRegularHospital, isAllRegularHospitalLoading, error } =
+  const { allRegularHospital, isAllRegularHospitalLoading, error: errorOnRegularHospitals } =
     useRegularHospital();
 
-  const onSubmit = (data) => {
-    const currentDateTime = createLocalDateTime();
-    data.dateTime = currentDateTime;
-    console.log(data);
-    console.log(errors);
-  };
-  const onError = () => {};
+  const { userRoleByUserRoleValue,
+    isUserRoleLoading,
+    error:errorOnCommonUserRole} =useCommonUserRole();
 
-  if (isAllRegularHospitalLoading) {
+    const { createUserProfile, isCreating } = useCreateUserProfile();
+  const onSubmit = (formData) => {
+    const userRoleId = userRoleByUserRoleValue.id;
+    const data = createData(formData,userRoleId);
+    createUserProfile(data);
+    reset();
+  };
+  const onError = (e) => {
+    toast.error("error occured please try again.");
+  };
+
+  if (isAllRegularHospitalLoading || isUserRoleLoading) {
     return <Spinner />;
   }
 
-  return (
-    <FormProvider {...methods}>
-      <Form onSubmit={handleSubmit(onSubmit, onError)}>
-        <UserProfilePersonalInformationForm />
-
-        <CustomFormRow
+  return(
+    <CreateUserProfileForm onSubmit={onSubmit} onError={onError} isCreating={isCreating}>
+      <CustomFormRow
           label="Select Responsible Hospital"
           error={errors?.hospitalId?.message}
         >
@@ -44,14 +54,8 @@ function CreateCommonUserProfile() {
             displayMessage="Select Hospital"
           ></SelectorWithinForm>
         </CustomFormRow>
-        <CustomFormRow>
-          <Button variation="primary" size="large" type="submit">
-            Submit
-          </Button>
-        </CustomFormRow>
-      </Form>
-    </FormProvider>
-  );
+      </CreateUserProfileForm>
+  )
 }
 
 export default CreateCommonUserProfile;
