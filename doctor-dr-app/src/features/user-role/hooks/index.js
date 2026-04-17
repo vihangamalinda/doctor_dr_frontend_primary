fffea7e800bc648a046f00c418ffc1cd0264b738
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-17 12:13:54 +0530

    feat: exposing the hooks related to user roles.

---
+++ doctor-dr-app/src/features/user-role/hooks/index.js
@@ -0,0 +1,7 @@
+import {useUserRoles} from "./useUserRoles.js";
+import {useUserRoleByUserRoleValue} from "./useUserRoleByUserRoleValue.js";
+
+export {
+    useUserRoles,
+    useUserRoleByUserRoleValue,
+}
