ff639f9dc67c67f6d1c6bcc77e0737cbe62e13e5
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-17 14:54:07 +0530

    refactor: renaming a file.

---
+++ doctor-dr-app/src/features/user-profile/CommonUserProfileTable.jsx
@@ -0,0 +1,26 @@
+import Row from "../../ui/secondary-ui/Row.jsx";
+import Spinner from "../../ui/secondary-ui/Spinner.jsx";
+import { useAllUserProfile } from "./hooks/useAllUserProfile.js";
+import UserProfileCard from "./UserProfileCard.jsx";
+import { useUserProfilesByUserRoleValue } from "./hooks/useUserProfilesByUserRoleValue.js";
+import { ROLE_CONSTANTS } from "../user-role/helper/index.js";
+
+function CommonUserProfileTable() {
+  /**
+   * TODO: need to retrive only common users not everyone
+   */
+  const { userProfilesByUserRoleValue,isUserProfilesLoading } = useUserProfilesByUserRoleValue(ROLE_CONSTANTS.commonUser);
+
+  if (isUserProfilesLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <Row type="vertical">
+      {userProfilesByUserRoleValue.map((userProfile) => (
+        <UserProfileCard userProfile={userProfile} key={userProfile.id} />
+      ))}
+    </Row>
+  );
+}
+export default CommonUserProfileTable;
