26da322ee175860111c0216e068f0b5944074a98
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-17 14:54:58 +0530

    feat: custom hook to retrive user profiles by User role values.

---
+++ doctor-dr-app/src/features/user-profile/hooks/useUserProfilesByUserRoleValue.js
@@ -0,0 +1,20 @@
+import { useQuery } from "@tanstack/react-query";
+import { getAllUserProfilesByUserRoleValue } from "../../../services/apis/apiUserProfile.js";
+import { GET_USER_PROFILES_BY_USER_ROLE_VALUE } from "./queryKeys.js";
+
+
+export function useUserProfilesByUserRoleValue(userRoleValue) {
+  const {
+    data: userProfilesByUserRoleValue,
+    isLoading: isUserProfilesLoading,
+    error,
+  } = useQuery({
+    queryKey: GET_USER_PROFILES_BY_USER_ROLE_VALUE(userRoleValue),
+    queryFn: () => getAllUserProfilesByUserRoleValue(userRoleValue),
+  });
+  return {
+    userProfilesByUserRoleValue,
+    isUserProfilesLoading,
+    error,
+  };
+}
