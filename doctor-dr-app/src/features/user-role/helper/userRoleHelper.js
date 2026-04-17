ea6b17bcaea605a452bd5e49772f78e3e5bb5304
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-17 12:13:22 +0530

    feat : custom hooks to get  userRole data.

---
+++ doctor-dr-app/src/features/user-role/helper/userRoleHelper.js
@@ -0,0 +1,39 @@
+import { useUserRoleByUserRoleValue } from "../hooks/index.js";
+
+const ROLE_CONSTANTS = {
+  admin: "ADMIN",
+  operationalStaff: "OPERATIONAL_STAFF",
+  commonUser: "COMMON_USER",
+};
+
+const useAdminRole = () => {
+  const { userRoleByUserRoleValue, isUserRoleLoading, error } =
+    useUserRoleByUserRoleValue(ROLE_CONSTANTS.admin);
+  return {
+    userRoleByUserRoleValue,
+    isUserRoleLoading,
+    error,
+  };
+};
+
+const useOperationalStaffRole = () => {
+  const { userRoleByUserRoleValue, isUserRoleLoading, error } =
+    useUserRoleByUserRoleValue(ROLE_CONSTANTS.operationalStaff);
+  return {
+    userRoleByUserRoleValue,
+    isUserRoleLoading,
+    error,
+  };
+};
+
+const useCommonUserRole = () => {
+  const { userRoleByUserRoleValue, isUserRoleLoading, error } =
+    useUserRoleByUserRoleValue(ROLE_CONSTANTS.commonUser);
+  return {
+    userRoleByUserRoleValue,
+    isUserRoleLoading,
+    error,
+  };
+};
+
+export { useAdminRole, useOperationalStaffRole, useCommonUserRole };
