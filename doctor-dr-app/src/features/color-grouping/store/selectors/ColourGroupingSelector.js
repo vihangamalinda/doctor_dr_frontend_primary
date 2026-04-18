4cf83c659816e1804c331735fd93314fd10bf3ce
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-18 20:50:09 +0530

    feat: selector for colour values.

---
+++ doctor-dr-app/src/features/color-grouping/store/selectors/ColourGroupingSelector.js
@@ -0,0 +1,16 @@
+export const selectCurrentColourGroup = (state) => state.colourGroup.currentColourGroup;
+
+export const selectAllThemesValues = (state) => {
+    const groups =state.colourGroup.colourGroups;
+    // console.log("groups",groups);
+    const valuearray =[groups.lightTheme.value, groups.darkTheme.value, groups.colourBlindFriendlyTheme.value];
+    
+    return valuearray;
+}
+
+export const selectLightTheme = (state) => 
+    state.colourGroup.colourGroups.lightTheme;
+export const selectDarkTheme = (state) => 
+    state.colourGroup.colourGroups.darkTheme;
+export const selectColourBlindFriendlyTheme = (state) => 
+    state.colourGroup.colourGroups.colourBlindFriendlyTheme;
