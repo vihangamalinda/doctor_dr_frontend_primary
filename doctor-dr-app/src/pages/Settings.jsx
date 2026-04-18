fd1ef6b3696a7386b83c9ac7006719d5eea248a7
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-18 20:52:16 +0530

    feat: integrating colour grouping selection to the settings. page.

--- doctor-dr-app/src/pages/Settings.jsx
+++ doctor-dr-app/src/pages/Settings.jsx
@@ -1,7 +1,11 @@
 // import Heading from "../ui/secondary-ui/Heading";
 import {Heading} from "../ui/index.js";
+import ColourSelections from "../features/color-grouping/ColourSelections.jsx";
 
 function Settings() {
-  return <Heading as="h1"> Settings</Heading>;
+  return <>
+  <Heading as="h1"> Settings</Heading>
+  <ColourSelections />
+  </>;
 }
 export default Settings;
