8431409e8240ddaed444a4b9134a4c58929175c4
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-18 20:51:10 +0530

    feat: exposing the colour grouping custom hooks.

---
+++ doctor-dr-app/src/features/color-grouping/hooks/index.js
@@ -0,0 +1,9 @@
+import {useUpdateColourBlindFriendlyTheme} from "./useUpdateColourBlindFriendlyTheme.js";
+import {useUpdateDarkTheme} from "./useUpdateDarkTheme.js";
+import {useUpdateLightTheme} from "./useUpdateLightTheme.js";
+
+export {
+    useUpdateLightTheme,
+    useUpdateDarkTheme,
+    useUpdateColourBlindFriendlyTheme,
+}
