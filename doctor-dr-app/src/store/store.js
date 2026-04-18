928d88934a8d22e76b9be3032c1287a593870113
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-18 20:51:30 +0530

    feat: integrating the colour grouping reducer to the store.

--- doctor-dr-app/src/store/store.js
+++ doctor-dr-app/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import currentLoggedUserReducer from "../features/authentication/store/useCurrentLoggedUser";
+import colourGroupReducer from "../features/color-grouping/store/useColourGroup"; 
 
 const store = configureStore({
   reducer: {
     currentLoggedUser: currentLoggedUserReducer,
+    colourGroup: colourGroupReducer,
   },
 });
 
