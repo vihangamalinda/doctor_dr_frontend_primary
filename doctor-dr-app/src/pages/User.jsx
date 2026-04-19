32fb03c5f0f65976f76227d91a47dd1cd6b9fea2
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-19 13:47:23 +0530

    feat: integrating the update password button.

--- doctor-dr-app/src/pages/User.jsx
+++ doctor-dr-app/src/pages/User.jsx
@@ -1,12 +1,14 @@
 // import Heading from "../ui/secondary-ui/Heading";
 import DisplayCurrentUserProfileInfromation from "../features/user-profile/DisplayCurrentUserProfileInformation.jsx";
 import {Heading} from "../ui/index.js";
+import {UpdatePasswordButton} from "../features/authentication/index.js";
 
 function User() {
   return (
     <>
       <Heading as="h1"> User details</Heading>
       <DisplayCurrentUserProfileInfromation />
+      <UpdatePasswordButton />
     </>
   );
 }
