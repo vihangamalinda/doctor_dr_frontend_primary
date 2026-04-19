00eba898701a5285c24fe877cd8ce1a3bf3f7cd4
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-19 13:46:42 +0530

    feat: update password pages.

---
+++ doctor-dr-app/src/pages/UpdateUserPassword.jsx
@@ -0,0 +1,8 @@
+import { UpdatePasswordForm } from "../features/authentication/index.js";
+
+function UpdateUserPassword() {
+    return (<UpdatePasswordForm />);
+
+}
+
+export default UpdateUserPassword;
