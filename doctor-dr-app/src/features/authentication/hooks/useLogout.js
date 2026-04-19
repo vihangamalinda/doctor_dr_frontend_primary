a2bf6e3bc22c5f662aebca46549c732dbff26100
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-19 10:38:03 +0530

    feat: custom hook related for logging out.

---
+++ doctor-dr-app/src/features/authentication/hooks/useLogout.js
@@ -0,0 +1,22 @@
+import { useMutation } from "@tanstack/react-query";
+import { destoyJwtTokenByUserLogout } from "../../../services/apis/apiUserCredentials.js";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+
+export function useLogout() {
+  const navigate = useNavigate();
+
+  const {mutate:logout,isLoading:isLoggingOut} = useMutation({
+    mutationFn: () => {
+      return destoyJwtTokenByUserLogout();
+    },
+    onSuccess: () => {
+      navigate("/login");
+    },
+    onError: (err) => {
+      console.log(err);
+      toast.error("Logout failed. Please try again.");
+    },
+  })
+return {logout,isLoggingOut};
+};
