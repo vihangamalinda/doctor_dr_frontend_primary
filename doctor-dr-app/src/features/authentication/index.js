b3ad147b4d8c685f2368894a82cf6466ce8cc8be
Vihanga Malinda <66559662+vihangamalinda@users.noreply.github.com>
2026-04-19 13:48:14 +0530

    feat: Exposing the components.

--- doctor-dr-app/src/features/authentication/index.js
+++ doctor-dr-app/src/features/authentication/index.js
@@ -1,5 +1,7 @@
 import LoginForm from "./LoginForm";
 import LogoutMiniButton from "./LogoutMiniButton";
 import Logout from "./Logout";
+import UpdatePasswordForm from "./UpdatePasswordForm";
+import UpdatePasswordButton from "./UpdatePasswordButton";
 
-export{ LoginForm, LogoutMiniButton, Logout };
+export{ LoginForm, LogoutMiniButton, Logout, UpdatePasswordForm, UpdatePasswordButton };
